1479d8026d2987c761de2c1547ecf2810d456998
Agustin <agustinperea5@gmail.com>
2024-06-12 16:30:29 -0300

    Inicializando proyecto

---
+++ src/components/Login.tsx
@@ -0,0 +1,7 @@
+export default function Login(params:type) {
+    return <form action="" method="post">
+        <h1>Iniciar sesion</h1>
+        <input type="email" name="" id="" />
+        <input type="password" name="" id="" />
+    </form>
+}
